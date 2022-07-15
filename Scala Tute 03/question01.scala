d408ff0344b5d47fe214f49ad1fc20e841419e55
Maleesha Madhuhansani <maleemapalagama@gmail.com>
2022-07-15 12:22:22 +0530

    Maleesha Commited

---
+++ Scala Tute 03/question01.scala
@@ -0,0 +1,8 @@
+object question01 extends App {
+
+  def area(r:Double):Double = math.Pi *r *r;
+
+  printf("Area of the disk is ");
+  println(area(5));
+
+}
---
+++ Scala Tute 03/question05.scala
@@ -0,0 +1,9 @@
+object question05 extends App {
+  def easyPace( a : Int):Int = 8 * a;
+
+  def  tempo( b : Int ):Int = 7 * b;
+
+  printf("Total running time is ");
+  print( easyPace(4) + tempo(3));
+  printf(" minutes. ");
+}
